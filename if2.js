0ef7a725a5b574704ab9892521d9c199dd86ee07
GAMELEIRA <gameleira270499@gmail.com>
2020-02-25 10:36:25 -0300

    Criado o arquivo if2.js que têm como objetivo trabalhar com possiveis erros com a estrutura if.

---
+++ if2.js
@@ -0,0 +1,31 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:25/02/2020 
+ * HORA:10:34
+ * FINALIDADE DO PROGRAMA: TRABALHAR COM POSSIVEIS ERROS DE CODIFICACAO COM A ESTRUTURA IF
+ */
+
+function teste1(num) {
+
+    if (num > 7) 
+        console.log(num);
+
+    console.log(`Final!`);
+
+}
+
+teste1(6);
+
+teste1(8);
+
+teste2(8);
+
+function teste2(num) {
+
+    if (num > 7) console.log(`Passou!`); {
+
+        console.log(num);
+
+    }
+
+}
