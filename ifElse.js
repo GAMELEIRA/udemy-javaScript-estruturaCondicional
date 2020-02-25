c72f8d33901c0a2ac3a4fc6b8c18dd009dfa1f79
GAMELEIRA <gameleira270499@gmail.com>
2020-02-25 10:44:02 -0300

    Criado o arquivo ifElse.js que tẽm como objetivo trabalhar com estruturas if(){}else{}.

---
+++ ifElse.js
@@ -0,0 +1,20 @@
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:25/02/2020 
+ * HORA:10:34
+ * FINALIDADE DO PROGRAMA: TRABALHAR COM POSSIVEIS ERROS DE CODIFICACAO COM A ESTRUTURA IF
+ */
+
+const imprimirResultado = function (nota) {
+
+    if (nota >= 7)
+        console.log(`Aprovado!`);
+    else
+        console.log(`Reprovado!`);
+}
+
+imprimirResultado(10);
+
+imprimirResultado(4);
+
+imprimirResultado(`Epa!`);
